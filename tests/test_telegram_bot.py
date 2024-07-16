4b2f6f641a247283b04e31b85a00127dd47e912d
Mohamad Shaheen <mhmd.shahin@outlook.com>
2024-07-16 15:31:37 +0300

    Added testing using pytest

--- tests/test_telegram_bot.py
+++ tests/test_telegram_bot.py
@@ -1,17 +1,22 @@
+import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 from server import app
 
 client = TestClient(app)
 
 
-def test_random_number_endpoint():
-    response = client.post('/telegram-bot/random-number/')
+@pytest.mark.asyncio
+async def test_random_number():
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        response = await ac.post('/telegram-bot/random-number/')
     assert response.status_code == 200
-    assert 'Number inserted' in response.text
+    assert 'Number inserted:' in response.text
 
 
-def test_random_string_endpoint():
-    test_string = 'Hello, world!'
-    response = client.post('/telegram-bot/string/', data={'string': test_string})
+@pytest.mark.asyncio
+async def test_random_string():
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        response = await ac.post('/telegram-bot/string/', data={'string': 'test string 1234'})
     assert response.status_code == 200
-    assert f'String inserted: {test_string}' in response.text
+    assert response.text == 'String inserted: test string 1234'
