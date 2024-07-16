8de483ecae0c09fefd242611f03f2f0f8f9846af
Mohamad Shaheen <mhmd.shahin@outlook.com>
2024-07-16 13:48:12 +0300

    Update telegram_bot.py
    Changed the mongodb_url

--- DAL/telegram_bot.py
+++ DAL/telegram_bot.py
@@ -9,7 +9,8 @@ load_dotenv()
 mongodb_username = quote_plus(os.getenv('MONGODB_USERNAME'))
 mongodb_password = quote_plus(os.getenv('MONGODB_PASSWORD'))
 
-mongodb_url = f'mongodb+srv://{mongodb_username}:{mongodb_password}@telegrambot.ztcvwep.mongodb.net/'
+# mongodb_url = f'mongodb+srv://{mongodb_username}:{mongodb_password}@telegrambot.ztcvwep.mongodb.net/'
+mongodb_url = f'mongodb+srv://{mongodb_username}:{mongodb_password}@telegrambot.ztcvwep.mongodb.net/?retryWrites=true&w=majority&appName=telegrambot'
 
 
 class TelegramBot:
